2f5e2f3a39f55c001bf476e3a174c705601f2d19
hongjian.chen <766256898@qq.com>
2018-12-05 15:11:42 +0800

    新增config server实例代码

---
+++ config-client/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by hongjian.chen on 2018/12/5.
+ */
+
+@RestController
+//@PropertySource(value = "classpath:application.yml",ignoreResourceNotFound = true)
+public class TestController {
+
+    @Value("${my.foo}")
+    private String foo;
+
+    @RequestMapping("/foo")
+    public String test() {
+        return foo;
+    }
+}
